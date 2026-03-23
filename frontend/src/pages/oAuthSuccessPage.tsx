55752ea9a11fd3eac26b73c2325e10b4b3ba4676
Sai Patel <saikumaraggu6@gmail.com>
2026-03-23 19:11:05 +0530

    fix:""updated route navigation oauth-sucess

--- frontend/src/pages/oAuthSuccessPage.tsx
+++ frontend/src/pages/oAuthSuccessPage.tsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function OAuthSuccess() {
+  const navigate = useNavigate();
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get("token");
 
     if (token) {
       localStorage.setItem("token", token);
-      window.location.href = "/";
+      navigate("/");
     } else {
-      window.location.href = "/login";
+      navigate("/login");
     }
   }, []);
 
